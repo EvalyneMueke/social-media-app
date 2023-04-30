19a5780f9839f54bb9cbb2f2133d955d50374779
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-05-01 00:11:46 +0300

    Finished up with user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+# app/controllers/sessions_controller.rb
+
+class SessionsController < ApplicationController
+    def create 
+        user = User.find_by(name:params[:name])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else 
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :sessions, only: [:create, :new, :destroy]
   resources :likes
   resources :comments
   resources :posts
   resources :users
+  
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
